23a4bc3346e1954569c6926f851a3f24224401a1
Daniel Krasovski <44982407+Meapy@users.noreply.github.com>
2021-09-24 17:01:48 +0100

    somewhat done`

--- GE1 Examples 2021/Assets/RotateMe.cs
+++ GE1 Examples 2021/Assets/RotateMe.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class RotateMe : MonoBehaviour
 {
-    
+    public GameObject prefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,6 @@ public class RotateMe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        transform.Rotate(1, 1, 1);
     }
 }
